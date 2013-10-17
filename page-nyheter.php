c86afe1c02ea15e9b3f95d53235fc61641f877a6
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2013-10-17 15:42:21 +0200

    huvudsida removed, small changes in page.php

--- page-nyheter.php
+++ page-nyheter.php
@@ -9,7 +9,7 @@
  */
 ?>
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
-<h1>Nyhetslista</h1>
+<h1>Nyheter</h1>
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 <h2><?php the_title(); ?></h2>
 <?php the_content(); ?>
--- page.php
+++ page.php
@@ -17,12 +17,15 @@
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/html-header', 'parts/shared/header' ) ); ?>
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-<?php /* slideshow */
-	echo HuGy::get_slideshow(get_field('hg_slideshow',get_the_ID()),'page slideshow','slideshow'); ?>
-
-<h1><?php the_title(); ?></h1>
-<?php the_content(); ?>
-<?php //comments_template( '', true ); ?>
+<div class="slideshow-wrapper">
+	<?php /* slideshow */ ?>
+	<?php echo HuGy::get_slideshow(get_field('hg_slideshow',get_the_ID()),'program slideshow','slideshow'); ?>
+</div>
+<div class="content-wrapper">
+	<h1><?php the_title(); ?></h1>
+	<?php the_content(); ?>
+	<?php //comments_template( '', true ); ?>
+</div>
 <?php endwhile; ?>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
