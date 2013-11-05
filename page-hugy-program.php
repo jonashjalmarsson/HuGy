cf0780ee5a313914b97b8ba2556ce15e4de0d5e1
Jonas Hjalmarsson <jonas.hjalmarsson@hultsfred.se>
2013-11-05 15:47:49 +0100

    bugfixes white top

--- page-hugy-program.php
+++ page-hugy-program.php
@@ -29,11 +29,17 @@
 	<?php echo HuGy::get_slideshow(get_field('hg_slideshow',get_the_ID()),'program slideshow','slideshow'); ?>
 </div>
 <div class="content-wrapper">
-	<h1><?php the_title(); ?></h1>
-	<?php the_content(); ?>
-	<?php //comments_template( '', true ); ?>
+	<div class="content">
+		<h1><?php the_title(); ?></h1>
+		<?php the_content(); ?>
+		<?php //comments_template( '', true ); ?>
+	</div>
+	<div class="block-wrapper">
+	<?php echo HuGy::get_contacts(get_the_ID()); ?><?php echo HuGy::get_related(get_the_ID()); ?>
+	</div>
+	<?php echo HuGy::get_modules(get_the_ID()); ?>
+
 </div>
-<?php echo HuGy::get_contacts(get_the_ID()); ?>
 <?php endwhile; ?>
 
 <?php Starkers_Utilities::get_template_parts( array( 'parts/shared/footer','parts/shared/html-footer' ) ); ?>
