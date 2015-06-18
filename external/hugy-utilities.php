6be06050440e5cf1fd126604a19ff3cc74047283
administrator <administrator@hks-webb-007.hks-webb-007.hultsfred.se>
2015-06-18 15:09:18 +0200

    gymnasium/komvux tags

--- external/hugy-utilities.php
+++ external/hugy-utilities.php
@@ -34,10 +34,6 @@
 		 
 			$retValue .= "<div class='quicklinks-wrapper'>";
 			$retValue .= "<ul class='quicklinks'>";
-			$retValue .= "<li class='quick-expand'>";
-			$retValue .= "<a title='Visa l&auml;nkar' href='#'>";
-			$retValue .= "<span class='quick-icon quick-expand-icon'></span>";
-			$retValue .= "</a></li>";
 		
 			while(has_sub_field('hg_quicklinks', 'option')):
 				$retValue .= "<li>";
@@ -328,9 +324,9 @@
 					'sort_column'  => 'post_title',
 					'link_before'  => '',
 					'link_after'   => '',
-					'walker'       => '',
 					'post_type'    => 'page',
 					'post_status'  => 'publish',
+					'walker'		=> new HuGy_Walker_Page()
 				); 
 				$retValue .= wp_list_pages( $args );
 			}
@@ -806,5 +802,92 @@
 			return $retValue;
 		}
 
+	}
+	/**
+	 * Create HTML list of pages.
+	 *
+	 * @since 2.1.0
+	 * @uses Walker
+	 */
+	class HuGy_Walker_Page extends Walker {
+	    
+	        public $db_fields = array ('parent' => 'post_parent', 'id' => 'ID');
+	
+	      
+	        public function start_lvl( &$output, $depth = 0, $args = array() ) {
+	                $indent = str_repeat("\t", $depth);
+	                $output .= "\n$indent<ul class='children'>\n";
+	        }
+	
+
+	        public function end_lvl( &$output, $depth = 0, $args = array() ) {
+	                $indent = str_repeat("\t", $depth);
+	                $output .= "$indent</ul>\n";
+	        }
+
+	        public function start_el( &$output, $page, $depth = 0, $args = array(), $current_page = 0 ) {
+	                if ( $depth ) {
+	                        $indent = str_repeat( "\t", $depth );
+	                } else {
+	                        $indent = '';
+	                }
+	
+	                $css_class = array( 'page_item', 'page-item-' . $page->ID );
+	
+	                if ( isset( $args['pages_with_children'][ $page->ID ] ) ) {
+	                        $css_class[] = 'page_item_has_children';
+	                }
+	
+	                if ( ! empty( $current_page ) ) {
+	                        $_current_page = get_post( $current_page );
+	                        if ( $_current_page && in_array( $page->ID, $_current_page->ancestors ) ) {
+	                                $css_class[] = 'current_page_ancestor';
+	                        }
+	                        if ( $page->ID == $current_page ) {
+	                                $css_class[] = 'current_page_item';
+	                        } elseif ( $_current_page && $page->ID == $_current_page->post_parent ) {
+	                                $css_class[] = 'current_page_parent';
+	                        }
+	                } elseif ( $page->ID == get_option('page_for_posts') ) {
+	                        $css_class[] = 'current_page_parent';
+	                }
+
+					
+	                $css_classes = implode( ' ', apply_filters( 'page_css_class', $css_class, $page, $depth, $args, $current_page ) );
+	
+	                if ( '' === $page->post_title ) {
+	                        $page->post_title = sprintf( __( '#%d (no title)' ), $page->ID );
+	                }
+	
+	                $args['link_before'] = empty( $args['link_before'] ) ? '' : $args['link_before'];
+	                $args['link_after'] = empty( $args['link_after'] ) ? '' : $args['link_after'];
+	
+	                /** This filter is documented in wp-includes/post-template.php */
+	                $output .= $indent . sprintf(
+	                        '<li class="%s">%s<a href="%s">%s%s%s</a>',
+	                        $css_classes,
+							HuGy::get_tags( $page->ID ),
+	                        get_permalink( $page->ID ),
+	                        $args['link_before'],
+	                        apply_filters( 'the_title', $page->post_title, $page->ID ),
+	                        $args['link_after']
+							);
+	
+	                if ( ! empty( $args['show_date'] ) ) {
+	                        if ( 'modified' == $args['show_date'] ) {
+	                                $time = $page->post_modified;
+	                        } else {
+	                                $time = $page->post_date;
+	                        }
+	
+	                        $date_format = empty( $args['date_format'] ) ? '' : $args['date_format'];
+	                        $output .= " " . mysql2date( $date_format, $time );
+	                }
+	        }
+
+	        public function end_el( &$output, $page, $depth = 0, $args = array() ) {
+	                $output .= "</li>\n";
+	        }
+	
 	}
  ?>
